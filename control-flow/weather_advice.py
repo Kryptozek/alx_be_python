0863d36c80af84cbb19017388dff23ad934411cf
Kryptozek <youngsammy87@gmail.com>
2024-09-22 20:17:00 +0300

    Removed brackets in if statement in weather_advice.p

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,15 @@
 #Ask the user for the weather
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 #Weather recommendation based
-if (weather == "sunny"):#If weather is sunny
+#If weather is sunny
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif(weather == "rainy"):#if weather  is rainy
+#if weather  is rainy
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif(weather == "cold"):#If weather is cold
+#If weather is cold
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
-else:#if response is different from expected
+#if response is different from expected
+else:
     print("Sorry, I don't have recommendations for this weather.")
