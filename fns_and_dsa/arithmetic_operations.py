d039d1a387bb63b685fc5131914619c5f7966d97
Kryptozek <youngsammy87@gmail.com>
2024-10-06 22:17:45 +0300

    Removed data types from function definition in arithmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@
 # This script defines a function that performs basic arithmetic operations (add, subtract, multiply, divide).
 # It also handles division by zero to avoid errors.
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
